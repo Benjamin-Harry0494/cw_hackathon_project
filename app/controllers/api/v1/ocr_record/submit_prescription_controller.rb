6cbab5363dd5f50980d613401a68eccbb30299f8
SusieCaro <susie.carovillano@patchwork.health>
2024-10-13 08:22:12 +0100

    :construction: - Pseudo code the wallet generation

--- app/controllers/api/v1/ocr_record/submit_prescription_controller.rb
+++ app/controllers/api/v1/ocr_record/submit_prescription_controller.rb
@@ -3,6 +3,9 @@ class Api::V1::OcrRecord::SubmitPrescriptionController < ApplicationController
 
   def create
     puts params
+
+    # TODO: put params into a call to GenerateWallet
+
     render json: { message: 'Hello world' }
   end
 end
