9b07b65f405bed59b9c16f42d74e0613ed036992
Benjamin-Harry0494 <Benjamin.harry@patchwork.health>
2024-10-13 09:19:37 +0100

    :construction: -- create generic instance and generate generic object

--- app/controllers/api/v1/ocr_record/create_entry_controller.rb
+++ app/controllers/api/v1/ocr_record/create_entry_controller.rb
@@ -10,27 +10,21 @@ class Api::V1::OcrRecord::CreateEntryController < ApplicationController
   def create
     puts params
     puts 'Creating new record'
-    new_record = build_ocr_record(params)
+    qr_code_data = build_ocr_record(params)
     patient_name = params[:patient_name]
     puts 'Attempting to create loyalty class'
-    result = create_loyalty_class(patient_name)
-    puts result.inspect
+    create_class_result = create_generic_class
+    puts create_class_result.inspect
+    
+    unless create_class_result[:success]
+      render json: { error: "Unable to create google loyalty class #{result.parsed_response}" },
+             status: :bad_request and return
+    end
 
-    #
-    # unless result.success?
-    #   render json: { error: "Unable to create google loyalty class #{result.parsed_response}" },
-    #          status: :bad_request and return
-    # end
-    #
-    # google_wallet_payload = {
-    #   iss: issuer_id,
-    #   aud: 'google',
-    #   typ: 'savetoandroidpay',
-    #   payload: {
-    #     ocr_record: new_record
-    #   }
-    # }
-    #
+    class_id = create_class_result[:loyalty_class_id]
+    create_instance_result = create_generic_instance(class_id, qr_code_data, patient_name)
+    puts create_instance_result.inspect
+
     # response = HTTParty.post(
     #   GOOGLE_WALLET_URL,
     #   body: google_wallet_payload.to_json,
@@ -59,31 +53,66 @@ class Api::V1::OcrRecord::CreateEntryController < ApplicationController
 
   private
 
-  def create_loyalty_class(patient_name)
+  def create_generic_class
     issuer_id = '3388000000022782190'
 
-    loyalty_class = WALLET_OBJECTS::LoyaltyClass.new(
-      id: "#{issuer_id}.loyaltyClass",
+    generic_class = WALLET_OBJECTS::GenericClass.new(
+      id: "#{issuer_id}.genericClass",
       issuer_name: 'Find My Eye Test',
-      program_name: 'Patient Eye Test History',
-      program_logo: WALLET_OBJECTS::Image.new(
-        source_uri: WALLET_OBJECTS::ImageUri.new(uri: 'https://cdn.vectorstock.com/i/500p/71/73/magnifier-with-eye-outline-icon-find-vector-31557173.jpg')
-      ),
-      reward_status: 'active',
+      title: 'Find My Eye Test',
+      provider: 'Find My Eye Test',
       review_status: 'draft',
-      redemption_channel: 'both',
-      text_modules: [
-        WALLET_OBJECTS::TextModuleData.new(
-          header: 'Patient Name',
-          body: patient_name
+      image_modules_data: [
+        WALLET_OBJECTS::ImageModuleData.new(
+          main_image: WALLET_OBJECTS::Image.new(
+            source_uri: WALLET_OBJECTS::ImageUri.new(uri: 'https://cdn.vectorstock.com/i/500p/71/73/magnifier-with-eye-outline-icon-find-vector-31557173.jpg')
+          )
         )
       ]
     )
 
-    @wallet_service.insert_loyaltyclass(loyalty_class)
+    res = @wallet_service.insert_genericclass(generic_class)
+    { success: true, message: 'Generic object created successfully', loyalty_class_id: "#{issuer_id}.offerClass", result: res }
   rescue Google::Apis::ClientError => e
-    Rails.logger.error("Failed to create loyalty class: #{e.message}")
-    raise
+    if e.message.include?("existingResource")
+      { success: true, message: 'Generic object already exists', loyalty_class_id: "#{issuer_id}.offerClass", result: res }
+    else
+      { success: false, message: 'Unable to create generic class', error: e.message, result: res }
+    end
+  end
+
+  def create_generic_instance(class_id, qr_code_data, patient_name)
+    generic_object = WALLET_OBJECTS::GenericObject.new(
+      id: class_id,
+      class_id: class_id,
+      state: 'active',
+      barcode: WALLET_OBJECTS::Barcode.new(
+        type: 'qrCode',
+        value: qr_code_data,
+        alternate_text: 'Scan for patient history'
+      ),
+      hero_image: WALLET_OBJECTS::Image.new(
+        source_uri: WALLET_OBJECTS::ImageUri.new(uri: 'https://cdn.vectorstock.com/i/500p/71/73/magnifier-with-eye-outline-icon-find-vector-31557173.jpg')
+      ),
+      text_modules_data: [
+        WALLET_OBJECTS::TextModuleData.new(
+          header: 'Patient Name',
+          body: patient_name
+        )
+      ],
+      links_module_data: WALLET_OBJECTS::LinksModuleData.new(
+        uris: [
+          WALLET_OBJECTS::Uri.new(
+            uri: 'https://yourwebsite.com/patient-history',
+            description: 'View full patient history'
+          )
+        ]
+      )
+    )
+    result = @wallet_service.insert_genericobject(generic_object)
+    { success: true, message: 'Generic object instance created successfully', result: result }
+  rescue Google::Apis::ClientError => e
+    { success: false, message: 'Failed to create generic object instance', error: e.message, result: result }
   end
 
   def generate_access_token
